4519ca063e653d35078fd7f1755ed3964cb0fc8c
mods456999 <kirmukhortikov@mail.ru>
2025-05-05 17:33:12 +0300

    Update service_install.bat

--- service_install.bat
+++ service_install.bat
@@ -114,3 +114,6 @@ sc delete %SRVCNAME%
 sc create %SRVCNAME% binPath= "\"%BIN_PATH%winws.exe\" %ARGS%" DisplayName= "zapret" start= auto
 sc description %SRVCNAME% "zapret program"
 sc start %SRVCNAME%
+nexus-cdn.com
+supporter-files.nexus-cdn.com
+files.nexus-cdn.com
